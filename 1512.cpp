28da3f8ac649bf4bc71ae43b99cea456682a1136
unknown <69777608+scmgustafson@users.noreply.github.com>
2021-02-23 14:34:46 -0800

    Solved

---
+++ 1512.cpp
@@ -0,0 +1,17 @@
+/* Runtime: 0 ms, faster than 100.00% of C++ online submissions for Number of Good Pairs.
+Memory Usage: 7.2 MB, less than 93.02% of C++ online submissions for Number of Good Pairs. */
+
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+        int good_pairs = 0;
+        for (int i = 0; i < nums.size(); i += 1) {
+            for (int j = 0; j < nums.size(); j += 1) {
+                if ((nums[i] == nums[j]) and (i < j)) {
+                    good_pairs += 1;
+                }
+            }
+        }
+        return good_pairs;
+    }
+};
