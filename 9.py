30ecf6a18bb58b6d3e432d6b1c31bb1557c52a13
unknown <69777608+scmgustafson@users.noreply.github.com>
2021-07-15 13:24:06 -0700

    second solve

--- 9.py
+++ 9.py
@@ -29,4 +29,31 @@ class Solution:
             
         return checker
             
+"""
+Palindrome Number
+
+2nd submission:
+Runtime: 80 ms, faster than 24.04% of Python3 online submissions for Palindrome Number.
+Memory Usage: 14.1 MB, less than 76.68% of Python3 online submissions for Palindrome Number.
+"""
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        x_str = str(x)
+        x_list = [i for i in x_str]
+
+        reversed_x_list = []
+        for i in range(len(x_list)-1, -1, -1):
+            reversed_x_list.append(x_list[i])
+        
+        reversed_x_str = ""
+        for i in reversed_x_list:
+            reversed_x_str += i
             
+        output = None
+        
+        if x_str == reversed_x_str:
+            output = True
+        else:
+            output = False
+        
+        return output
